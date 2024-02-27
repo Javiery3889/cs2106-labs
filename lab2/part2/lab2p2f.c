d63752b92062d08166b84920446e096394f0e41e
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2024-02-27 22:12:05 +0800

    working lab 2 part 2 f q2.7 initial

--- lab2/part2/lab2p2f.c
+++ lab2/part2/lab2p2f.c
@@ -15,6 +15,43 @@ int main() {
     // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
     // WITHOUT using | and > from the shell.
     //
+    
+    // slow sout>pipe>sin talk >pipe? 
+    
+    // 1: FD for result file
+    int fp_out = open("./results.out", O_CREAT | O_WRONLY);
 
+    // p[0] read, p[1] write
+    int p[2];
+    if (pipe(p) <0) {
+        perror("lab2pf2c: ");
+    }
+    
+    if (fork() != 0) {
+        // parent
+        close(p[1]); // close write
+        wait(NULL); // wait for child to send 
+        dup2(p[0], STDIN_FILENO);
+        dup2(fp_out, STDOUT_FILENO);
+        if (fork() == 0) {
+            execlp("./talk", "talk", (char *) 0);
+        } else {
+            wait(NULL);
+            close(fp_out);
+            close(p[0]);
+        }
+    } else {
+        close(p[0]); // close read
+        dup2(p[1], STDOUT_FILENO); // stdout now goes to write pipe
+        if (fork() == 0) {
+            // execlp("./slow", "slow", (char *) 0);
+            execlp("./slow", "slow", "5", NULL);
+        } else {
+            wait(NULL);
+            // write(p[1], 1, EOF);
+            close(p[1]);
+        }
+        // write(p[1], NULL, 1);
+    }
 }
 
