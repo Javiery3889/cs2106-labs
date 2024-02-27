2ec0b9557d6cec0c80d5db5d0e9756ef47e675f5
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2024-02-27 22:22:42 +0800

    Improve code to make it more simple

--- lab2/part2/lab2p2f.c
+++ lab2/part2/lab2p2f.c
@@ -27,31 +27,19 @@ int main() {
         perror("lab2pf2c: ");
     }
     
+    //  -bash
+    //   \_ talk
+    //       \_ slow 5
     if (fork() != 0) {
         // parent
         close(p[1]); // close write
-        wait(NULL); // wait for child to send 
-        dup2(p[0], STDIN_FILENO);
-        dup2(fp_out, STDOUT_FILENO);
-        if (fork() == 0) {
-            execlp("./talk", "talk", (char *) 0);
-        } else {
-            wait(NULL);
-            close(fp_out);
-            close(p[0]);
-        }
+        dup2(p[0], STDIN_FILENO); // stdin in read
+        dup2(fp_out, STDOUT_FILENO); // stdout is file
+        execlp("./talk", "talk", (char *) 0);
     } else {
         close(p[0]); // close read
         dup2(p[1], STDOUT_FILENO); // stdout now goes to write pipe
-        if (fork() == 0) {
-            // execlp("./slow", "slow", (char *) 0);
-            execlp("./slow", "slow", "5", NULL);
-        } else {
-            wait(NULL);
-            // write(p[1], 1, EOF);
-            close(p[1]);
-        }
-        // write(p[1], NULL, 1);
+        execlp("./slow", "slow", "5", NULL);
     }
 }
 
