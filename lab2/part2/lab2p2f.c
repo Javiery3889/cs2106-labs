0bcbee9cf19082f5ef942a826b9d0c0f300122cf
Javier <javiery3889@gmail.com>
2024-03-04 10:33:43 +0800

    Fix saved file permission settings

--- lab2/part2/lab2p2f.c
+++ lab2/part2/lab2p2f.c
@@ -4,42 +4,40 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+#define READ_END 0
+#define WRITE_END 1
+
 int main() {
 
     printf("Be patient, the program will take around 7 seconds to run.\n");
     printf("At the end you can do \"cat results.out\" to see the result.\n");
 
-    //
-    // Add code here to pipe from ./slow 5 to ./talk and redirect
-    // output of ./talk to results.out
-    // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
-    // WITHOUT using | and > from the shell.
-    //
-    
-    // slow sout>pipe>sin talk >pipe? 
-    
-    // 1: FD for result file
-    int fp_out = open("./results.out", O_CREAT | O_WRONLY);
-
-    // p[0] read, p[1] write
-    int p[2];
-    if (pipe(p) <0) {
-        perror("lab2pf2c: ");
+    int fp_out = open("./result.out", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fp_out < 0) {
+        perror("Failed to open result.out");
+        exit(EXIT_FAILURE);
     }
-    
-    //  -bash
-    //   \_ talk
-    //       \_ slow 5
-    if (fork() != 0) {
-        // parent
-        close(p[1]); // close write
-        dup2(p[0], STDIN_FILENO); // stdin in read
-        dup2(fp_out, STDOUT_FILENO); // stdout is file
-        execlp("./talk", "talk", (char *) 0);
-    } else {
-        close(p[0]); // close read
-        dup2(p[1], STDOUT_FILENO); // stdout now goes to write pipe
+
+    int pipe_fd[2];
+    if (pipe(pipe_fd) < 0) {
+        perror("lab2p2f: ");
+    }
+
+    int pid = fork();
+    // child execute ./slow, parent execute ./talk
+    if (pid == 0) {
+        close(pipe_fd[READ_END]);
+        // stdout now goes to pipe write end
+        dup2(pipe_fd[WRITE_END], STDOUT_FILENO);
         execlp("./slow", "slow", "5", NULL);
+    } else {
+        close(pipe_fd[WRITE_END]);
+        // we always wait for child, i.e. ./slow to finish executing first
+        waitpid(pid, NULL, WEXITED);
+        // stdin now goes to pipe read end
+        dup2(pipe_fd[READ_END], STDIN_FILENO);
+        dup2(fp_out, STDOUT_FILENO);
+        execlp("./talk", "talk", NULL);
     }
 }
 
