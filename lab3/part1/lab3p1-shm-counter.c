3481950354c622425f3b9551e89ed2903b5166f0
Javier <javiery3889@gmail.com>
2024-03-24 10:14:35 +0800

    Update lab 3 part 1 to use semaphore array
    - Semaphore array used to maintain order of child processes
    - Mutex used to check if counter is available

--- lab3/part1/lab3p1-shm-counter.c
+++ lab3/part1/lab3p1-shm-counter.c
@@ -9,7 +9,10 @@
 #define NUM_CHILDREN 5
 
 struct shared_memory {
-    sem_t sem;
+    // sem array is indicate which process' turn is it based on index
+    sem_t sem[NUM_CHILDREN];
+    // mutex is used to indicate if counter variable is available
+    sem_t mutex;
     int counter;
 };
 
@@ -32,10 +35,17 @@ int main() {
         exit(1);
     }
 
-    // Initialise semaphore's value to 1, so that first child does not wait indefinitely
+    // counter resource is available initially
+    sem_init(&(shm->mutex), 1, 1);
+    // set all child process to block when executed
+    for (i = 0; i < NUM_CHILDREN; i++) {
+        sem_init(&(shm->sem[i]), 1, 0);
+    }
     // initialise counter variable to 0 as well
-    sem_init(&(shm->sem), 1, 1);
     shm->counter = 0;
+
+    // only allow child 1 to execute critical section
+    sem_post(&(shm->sem[0]));
 
     for (i = 0; i < NUM_CHILDREN; i++) {
         pid = fork();
@@ -47,11 +57,13 @@ int main() {
         perror("fork");
         exit(EXIT_FAILURE);
     }
-        
-    // Child process
+
     else if (pid == 0) {
-        // Lock semaphore
-        sem_wait(&(shm->sem));
+        // block any other child process
+        sem_wait(&(shm->sem[i]));
+        // lock mutex 
+        sem_wait(&(shm->mutex));
+        // Child process
         printf("Child %d starts\n", i + 1);
         // Simulate some work
         for (int j = 0; j < 5; j++) {
@@ -63,20 +75,22 @@ int main() {
             usleep(250000);
         }
         printf("Child %d finishes with counter %d\n", i + 1, shm->counter);
-        // Unlock semaphore
-        sem_post(&(shm->sem));
+        // unlock mutex
+        sem_post(&(shm->mutex));
+        // next child can be unblocked
+        sem_post(&(shm->sem[i+1]));
         exit(EXIT_SUCCESS);
     }
 
     // Parent process
     for (int i = 0; i < NUM_CHILDREN; i++) {
         wait(NULL);
+        sem_destroy(&(shm->sem[i]));
     }
 
     // Print the final value of the counter
     printf("Final counter value: %d\n", shm->counter);
 
-    sem_destroy(&(shm->sem));
     // Detach and destroy the shared memory region
     shmdt(shm);
     shmctl(shmid, IPC_RMID, 0);
